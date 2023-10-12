3ad113b7a7f11479fb7d097996b6a3530f71ea29
Yohannes <uelandrae@gmail.com>
2023-10-12 20:29:37 +0200

    deleted backend is recovered

--- admin/src/components/charts/pieChart/Piechart.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Piechart = () => {
-  return (
-    <div>Piechart</div>
-  )
-}
-
-export default Piechart
--- admin/src/components/messages/MessageBox.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Alert from 'react-bootstrap/Alert';
-
-const MessageBox = (color) => {
-  return <Alert variant={color.variant || 'info'}>{color.children}</Alert>;
-};
-
-export default MessageBox;
